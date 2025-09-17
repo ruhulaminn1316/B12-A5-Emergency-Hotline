fbcfbce0b0c537aa954e4daba7b8d9bdd6e4a1ae
RUHUL AMIN <ruhulaminn1316@gmail.com>
2025-09-17 23:48:37 +0600

    js file update

--- js/function.js
+++ js/function.js
@@ -1,1 +1,20 @@
-console.log(5+10);
+let dataHistory = [];
+
+
+function getInnrTxt(str){
+    const result = parseInt(document.getElementById(str).innerText);
+    return result;
+}
+
+
+function stInnrTxt(str1, value ){
+const setTextId = document.getElementById(str1);
+setTextId.innerText = value;
+}
+
+function historyAdd(){
+const div = document.createElement("div");
+
+
+
+}
